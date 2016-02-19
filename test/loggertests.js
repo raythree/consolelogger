3aa85db31ce83378ac667268f1fa3c9ad23cf006
raythree <github@raythree.com>
2016-02-19 18:49:53 -0500

    working revs

--- test/loggertests.js
+++ test/loggertests.js
@@ -10,18 +10,51 @@ describe('console logger tests', function () {
     assert(typeof logger.getCategoryLevel === 'function')
   })
 
-  it('should support configuration', function () {
-    logger.configure({
-      level: 'debug'
-    })
+  it('should support level configuration', function () {
+    logger.configure({level: 'off'})
+    assert(logger.getLevel() === 'off')
+
+    logger.configure({level: 'trace'})
+    assert(logger.getLevel() === 'trace')
+
+    logger.configure({level: 'debug'})
     assert(logger.getLevel() === 'debug')
-    assert(logger.getCategoryLevel('none') === 'off')
 
-    logger.configure({
-      level: 'all'
-    })
-    assert(logger.getLevel() === 'all')
+    logger.configure({level: 'info'})
+    assert(logger.getLevel() === 'info')
 
+    logger.configure({level: 'warn'})
+    assert(logger.getLevel() === 'warn')
+
+    logger.configure({level: 'error'})
+    assert(logger.getLevel() === 'error')
+
+    logger.configure({level: 'fatal'})
+    assert(logger.getLevel() === 'fatal')
   })
 
+  it('should support category configuration', function () {
+    logger.configure({
+      level: 'info',
+      levels: {
+        category1: 'debug',
+        category2: 'error'
+      }
+    })
+    assert(logger.getLevel() === 'info')
+    assert(logger.getCategoryLevel('category1') === 'debug')
+    assert(logger.getCategoryLevel('category2') === 'error')
+    assert(logger.getCategoryLevel('notfound') === 'info')  // default
+  })
+
+  it('should return the default logger', function () {
+    logger.configure({
+      level: 'info',
+      levels: {
+        category1: 'debug',
+        category2: 'error'
+      }
+    })
+    logger.getLogger()
+  })
 })
