d35be30989c7dbc39cf2133aa0fd7487e8123da8
Bill Walker <bill@IPKeys-MacBook-Pro-2.local>
2016-02-19 17:31:59 -0500

    working revs

--- test/loggertests.js
+++ test/loggertests.js
@@ -2,7 +2,26 @@ var assert = require('assert')
 var logger = require('../../consolelogger')
 
 describe('console logger tests', function () {
-  it('should default to info level', function () {
-    assert(true)
+
+  it('should export the right functions', function () {
+    assert(typeof logger.getLogger === 'function')
+    assert(typeof logger.configure === 'function')
+    assert(typeof logger.getLevel === 'function')
+    assert(typeof logger.getCategoryLevel === 'function')
   })
+
+  it('should support configuration', function () {
+    logger.configure({
+      level: 'debug'
+    })
+    assert(logger.getLevel() === 'debug')
+    assert(logger.getCategoryLevel('none') === 'off')
+
+    logger.configure({
+      level: 'all'
+    })
+    assert(logger.getLevel() === 'all')
+
+  })
+
 })
